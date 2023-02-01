29b5cdb43b9f69e7948fb7182bb57ebe7dad2b85
Iz0nite <armanddfl@gmail.com>
2023-02-01 09:57:29 +0100

    fix: clean-migration

---
+++ migrations/Version20230201085417.php
@@ -0,0 +1,118 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20230201085417 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE SEQUENCE document_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
+        $this->addSql('CREATE SEQUENCE equipment_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
+        $this->addSql('CREATE SEQUENCE message_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
+        $this->addSql('CREATE SEQUENCE mission_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
+        $this->addSql('CREATE SEQUENCE "order_id_seq" INCREMENT BY 1 MINVALUE 1 START 1');
+        $this->addSql('CREATE SEQUENCE rating_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
+        $this->addSql('CREATE SEQUENCE step_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
+        $this->addSql('CREATE SEQUENCE type_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
+        $this->addSql('CREATE SEQUENCE "user_id_seq" INCREMENT BY 1 MINVALUE 1 START 1');
+        $this->addSql('CREATE TABLE document (id INT NOT NULL, submited_by_id INT NOT NULL, path VARCHAR(255) DEFAULT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE INDEX IDX_D8698A7649151F17 ON document (submited_by_id)');
+        $this->addSql('CREATE TABLE equipment (id INT NOT NULL, name VARCHAR(255) NOT NULL, description TEXT NOT NULL, stock INT NOT NULL, slug VARCHAR(105) NOT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL, updated_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE TABLE message (id INT NOT NULL, mission_id_id INT NOT NULL, from_id_id INT NOT NULL, content VARCHAR(1024) NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE INDEX IDX_B6BD307FEFD2C16A ON message (mission_id_id)');
+        $this->addSql('CREATE INDEX IDX_B6BD307F4632BB48 ON message (from_id_id)');
+        $this->addSql('CREATE TABLE mission (id INT NOT NULL, type_id INT DEFAULT NULL, client_id INT NOT NULL, agent_id INT DEFAULT NULL, name VARCHAR(100) NOT NULL, slug VARCHAR(105) NOT NULL, description TEXT NOT NULL, image VARCHAR(255) DEFAULT NULL, status VARCHAR(20) NOT NULL, reward DOUBLE PRECISION NOT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL, updated_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE INDEX IDX_9067F23CC54C8C93 ON mission (type_id)');
+        $this->addSql('CREATE INDEX IDX_9067F23C19EB6921 ON mission (client_id)');
+        $this->addSql('CREATE INDEX IDX_9067F23C3414710B ON mission (agent_id)');
+        $this->addSql('CREATE TABLE "order" (id INT NOT NULL, agent_id INT NOT NULL, mission_id INT NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE INDEX IDX_F52993983414710B ON "order" (agent_id)');
+        $this->addSql('CREATE INDEX IDX_F5299398BE6CAE90 ON "order" (mission_id)');
+        $this->addSql('CREATE TABLE order_equipment (order_id INT NOT NULL, equipment_id INT NOT NULL, PRIMARY KEY(order_id, equipment_id))');
+        $this->addSql('CREATE INDEX IDX_6FBFAE7B8D9F6D38 ON order_equipment (order_id)');
+        $this->addSql('CREATE INDEX IDX_6FBFAE7B517FE9FE ON order_equipment (equipment_id)');
+        $this->addSql('CREATE TABLE rating (id INT NOT NULL, agent_id INT NOT NULL, rate INT NOT NULL, opinion TEXT DEFAULT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE INDEX IDX_D88926223414710B ON rating (agent_id)');
+        $this->addSql('CREATE TABLE step (id INT NOT NULL, mission_id INT NOT NULL, name VARCHAR(255) NOT NULL, status BOOLEAN NOT NULL, position INT NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE INDEX IDX_43B9FE3CBE6CAE90 ON step (mission_id)');
+        $this->addSql('CREATE TABLE type (id INT NOT NULL, name VARCHAR(50) NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE TABLE "user" (id INT NOT NULL, email VARCHAR(180) NOT NULL, roles JSON NOT NULL, nickname VARCHAR(255) NOT NULL, firstname VARCHAR(255) NOT NULL, lastname VARCHAR(255) NOT NULL, password VARCHAR(255) NOT NULL, description VARCHAR(1024) DEFAULT NULL, status BOOLEAN NOT NULL, validation_token VARCHAR(64) NOT NULL, email_notify BOOLEAN NOT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL, updated_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_8D93D649E7927C74 ON "user" (email)');
+        $this->addSql('CREATE TABLE messenger_messages (id BIGSERIAL NOT NULL, body TEXT NOT NULL, headers TEXT NOT NULL, queue_name VARCHAR(190) NOT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, available_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, delivered_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE INDEX IDX_75EA56E0FB7336F0 ON messenger_messages (queue_name)');
+        $this->addSql('CREATE INDEX IDX_75EA56E0E3BD61CE ON messenger_messages (available_at)');
+        $this->addSql('CREATE INDEX IDX_75EA56E016BA31DB ON messenger_messages (delivered_at)');
+        $this->addSql('CREATE OR REPLACE FUNCTION notify_messenger_messages() RETURNS TRIGGER AS $$
+            BEGIN
+                PERFORM pg_notify(\'messenger_messages\', NEW.queue_name::text);
+                RETURN NEW;
+            END;
+        $$ LANGUAGE plpgsql;');
+        $this->addSql('DROP TRIGGER IF EXISTS notify_trigger ON messenger_messages;');
+        $this->addSql('CREATE TRIGGER notify_trigger AFTER INSERT OR UPDATE ON messenger_messages FOR EACH ROW EXECUTE PROCEDURE notify_messenger_messages();');
+        $this->addSql('ALTER TABLE document ADD CONSTRAINT FK_D8698A7649151F17 FOREIGN KEY (submited_by_id) REFERENCES "user" (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('ALTER TABLE message ADD CONSTRAINT FK_B6BD307FEFD2C16A FOREIGN KEY (mission_id_id) REFERENCES mission (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('ALTER TABLE message ADD CONSTRAINT FK_B6BD307F4632BB48 FOREIGN KEY (from_id_id) REFERENCES "user" (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('ALTER TABLE mission ADD CONSTRAINT FK_9067F23CC54C8C93 FOREIGN KEY (type_id) REFERENCES type (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('ALTER TABLE mission ADD CONSTRAINT FK_9067F23C19EB6921 FOREIGN KEY (client_id) REFERENCES "user" (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('ALTER TABLE mission ADD CONSTRAINT FK_9067F23C3414710B FOREIGN KEY (agent_id) REFERENCES "user" (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('ALTER TABLE "order" ADD CONSTRAINT FK_F52993983414710B FOREIGN KEY (agent_id) REFERENCES "user" (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('ALTER TABLE "order" ADD CONSTRAINT FK_F5299398BE6CAE90 FOREIGN KEY (mission_id) REFERENCES mission (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('ALTER TABLE order_equipment ADD CONSTRAINT FK_6FBFAE7B8D9F6D38 FOREIGN KEY (order_id) REFERENCES "order" (id) ON DELETE CASCADE NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('ALTER TABLE order_equipment ADD CONSTRAINT FK_6FBFAE7B517FE9FE FOREIGN KEY (equipment_id) REFERENCES equipment (id) ON DELETE CASCADE NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('ALTER TABLE rating ADD CONSTRAINT FK_D88926223414710B FOREIGN KEY (agent_id) REFERENCES "user" (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('ALTER TABLE step ADD CONSTRAINT FK_43B9FE3CBE6CAE90 FOREIGN KEY (mission_id) REFERENCES mission (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE SCHEMA public');
+        $this->addSql('DROP SEQUENCE document_id_seq CASCADE');
+        $this->addSql('DROP SEQUENCE equipment_id_seq CASCADE');
+        $this->addSql('DROP SEQUENCE message_id_seq CASCADE');
+        $this->addSql('DROP SEQUENCE mission_id_seq CASCADE');
+        $this->addSql('DROP SEQUENCE "order_id_seq" CASCADE');
+        $this->addSql('DROP SEQUENCE rating_id_seq CASCADE');
+        $this->addSql('DROP SEQUENCE step_id_seq CASCADE');
+        $this->addSql('DROP SEQUENCE type_id_seq CASCADE');
+        $this->addSql('DROP SEQUENCE "user_id_seq" CASCADE');
+        $this->addSql('ALTER TABLE document DROP CONSTRAINT FK_D8698A7649151F17');
+        $this->addSql('ALTER TABLE message DROP CONSTRAINT FK_B6BD307FEFD2C16A');
+        $this->addSql('ALTER TABLE message DROP CONSTRAINT FK_B6BD307F4632BB48');
+        $this->addSql('ALTER TABLE mission DROP CONSTRAINT FK_9067F23CC54C8C93');
+        $this->addSql('ALTER TABLE mission DROP CONSTRAINT FK_9067F23C19EB6921');
+        $this->addSql('ALTER TABLE mission DROP CONSTRAINT FK_9067F23C3414710B');
+        $this->addSql('ALTER TABLE "order" DROP CONSTRAINT FK_F52993983414710B');
+        $this->addSql('ALTER TABLE "order" DROP CONSTRAINT FK_F5299398BE6CAE90');
+        $this->addSql('ALTER TABLE order_equipment DROP CONSTRAINT FK_6FBFAE7B8D9F6D38');
+        $this->addSql('ALTER TABLE order_equipment DROP CONSTRAINT FK_6FBFAE7B517FE9FE');
+        $this->addSql('ALTER TABLE rating DROP CONSTRAINT FK_D88926223414710B');
+        $this->addSql('ALTER TABLE step DROP CONSTRAINT FK_43B9FE3CBE6CAE90');
+        $this->addSql('DROP TABLE document');
+        $this->addSql('DROP TABLE equipment');
+        $this->addSql('DROP TABLE message');
+        $this->addSql('DROP TABLE mission');
+        $this->addSql('DROP TABLE "order"');
+        $this->addSql('DROP TABLE order_equipment');
+        $this->addSql('DROP TABLE rating');
+        $this->addSql('DROP TABLE step');
+        $this->addSql('DROP TABLE type');
+        $this->addSql('DROP TABLE "user"');
+        $this->addSql('DROP TABLE messenger_messages');
+    }
+}
