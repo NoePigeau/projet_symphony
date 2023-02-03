ef4bb04ed53a33cb09d6747daa5b866e8049e18f
Antoine <antoine.lorin2@gmail.com>
2023-02-03 18:23:04 +0100

    security(skills): update skills (#36)

---
+++ src/Form/UserSkill.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Type;
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class SkillType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('type', EntityType::class, [
+                'class' => Type::class,
+                'label' => 'Skill',
+                'multiple' => true,
+                'choice_label'  => function ($type) {
+                    return $type->getName();
+                }])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class
+        ]);
+    }
+}
