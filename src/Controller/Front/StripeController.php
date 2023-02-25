db4f359941e7220afd0c6c6760e2bdbf7e199d28
Théo Hervé <theojherve@gmail.com>
2023-02-25 15:31:56 +0100

    Notify client and agent when payment (if they agreed) (#62)
    Implement Stripe API + add history page
    
    Fix doublon payment
    
    Notify client and agent when payment
    
    Implement Stripe API + add history page
    
    Fix doublon payment
    
    Notify client and agent when payment
    
    Implement Stripe API + add history page
    
    Fix doublon payment
    
    Notify client and agent when payment
    
    Notify client and agent when payment (if they agreed + fix)
    
    Notify client and agent when payment (if they agreed before + fix)
    
    Notify client and agent when payment (if they agreed + fix)
    
    Notify client and agent when payment (if they agreed)
    
    Implement Stripe API + add history page
    
    Fix doublon payment
    
    Notify client and agent when payment
    
    Implement Stripe API + add history page
    
    Fix doublon payment
    
    Notify client and agent when payment
    
    Implement Stripe API + add history page
    
    Fix doublon payment
    
    Notify client and agent when payment
    
    Notify client and agent when payment (if they agreed + fix)
    
    Notify client and agent when payment (if they agreed before + fix)
    
    Notify client and agent when payment (if they agreed)
    
    Implement Stripe API + add history page
    
    Fix doublon payment
    
    Notify client and agent when payment
    
    Implement Stripe API + add history page
    
    Fix doublon payment
    
    Notify client and agent when payment
    
    Implement Stripe API + add history page
    
    Fix doublon payment
    
    Notify client and agent when payment
    
    Notify client and agent when payment (if they agreed + fix)
    
    Notify client and agent when payment (if they agreed before + fix)
    
    Notify client and agent when payment (if they agreed + fix)

--- src/Controller/Front/StripeController.php
+++ src/Controller/Front/StripeController.php
@@ -9,6 +9,9 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Stripe\Exception\ApiErrorException;
 use Stripe\StripeClient;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\Exception\TransportExceptionInterface;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
@@ -51,26 +54,26 @@ class StripeController extends AbstractController
 		
 		// Create a new Stripe Checkout session
 		$session = $stripe->checkout->sessions->create([
-	        'payment_method_types' => ['card'],
-	        'line_items' => [[
-	            'price_data' => [
-	                'currency' => 'eur',
-	                'unit_amount' => $payment->getAmount(),
-	                'product_data' => [
-	                    'name' => 'Mission Reward',
-	                    'description' => $mission->getDescription()
-	                ],
-	            ],
-	            'quantity' => 1,
-	        ]],
-	        'mode' => 'payment',
-	        'success_url' => $this->generateUrl('front_payment_success', ['paymentId' => $payment->getId()], UrlGeneratorInterface::ABSOLUTE_URL),
-	        'cancel_url' => $this->generateUrl('front_payment_cancel', ['paymentId' => $payment->getId()], UrlGeneratorInterface::ABSOLUTE_URL),
-	        'metadata' => [
-	            'payment_id' => $payment->getId(),
-	            'mission_id' => $mission->getId()
-	        ]
-		]);
+           'payment_method_types' => ['card'],
+           'line_items' => [[
+               'price_data' => [
+                   'currency' => 'eur',
+                   'unit_amount' => $payment->getAmount(),
+                   'product_data' => [
+                       'name' => 'Mission Reward',
+                       'description' => $mission->getDescription()
+                   ],
+               ],
+               'quantity' => 1,
+           ]],
+           'mode' => 'payment',
+           'success_url' => $this->generateUrl('front_payment_success', ['paymentId' => $payment->getId()], UrlGeneratorInterface::ABSOLUTE_URL),
+           'cancel_url' => $this->generateUrl('front_payment_cancel', ['paymentId' => $payment->getId()], UrlGeneratorInterface::ABSOLUTE_URL),
+           'metadata' => [
+               'payment_id' => $payment->getId(),
+               'mission_id' => $mission->getId()
+           ]
+        ]);
 		
 		// Save the Stripe session ID to the Payment object and update it in the database
 		$payment->setStripePaymentId($session->id);
@@ -92,10 +95,11 @@ class StripeController extends AbstractController
 	
 	/**
 	 * @Route("/payment/success/{paymentId}", name="payment_success")
+	 * @throws TransportExceptionInterface
 	 */
 	#[Route('/payment/success/{paymentId}', name: 'payment_success', methods: ['GET'])]
 	#[Security("is_granted('ROLE_CLIENT')")]
-	public function paymentSuccess(EntityManagerInterface $entityManager, int $paymentId): Response
+	public function paymentSuccess(EntityManagerInterface $entityManager, int $paymentId, MailerInterface $mailer): Response
 	{
 		// Retrieve the payment from the database using the paymentId
 		$payment = $entityManager->getRepository(Payment::class)->find($paymentId);
@@ -103,6 +107,28 @@ class StripeController extends AbstractController
 		// Update the payment status to "paid"
 		$payment->setStatus('paid');
 		$entityManager->flush();
+		
+		$mission = $payment->getMission();
+		
+		// Send an e-mail to the client to notify him that the payment is successful
+		if ($mission->getClient()->getEmailNotify()) {
+			$emailToClient = (new Email())
+				->from('mission-bot@kgbytes.com')
+				->to($mission->getClient()->getEmail())
+				->subject('You have pad for your mission')
+				->html('<p>' . $mission->getClient()->getNickname() . ' You have successfully paid for the mission: ' . $mission->getName() .'. Thank you for choosing us !</p>');
+			$mailer->send($emailToClient);
+		}
+		
+		// Send an e-mail to the agent to notify him that the payment is successful
+		if ($mission->getAgent()->getEmailNotify()) {
+			$emailToAgent = (new Email())
+				->from('mission-bot@kgbytes.com')
+				->to($mission->getAgent()->getEmail())
+				->subject('You have received a payment for your mission')
+				->html('<p>' . $mission->getAgent()->getNickname() . ' You have successfully received a payment for the mission: ' . $mission->getName() .'. Keep up the good work !</p>');
+			$mailer->send($emailToAgent);
+		}
 		
 		// Redirect the user to the payment success page
 		return $this->redirectToRoute('front_payment_success_page');
